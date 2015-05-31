87e5c9619ab17bc05c64fac271aae5aa09587c53
abassawo <abass.bayo@gmail.com>
2015-05-31 16:20:35 -0400

    resolving additional merge conf

--- Meme-ify-Me/app/src/main/java/nyc/c4q/rosmaryfc/meme_ify_me/MainActivity.java
+++ Meme-ify-Me/app/src/main/java/nyc/c4q/rosmaryfc/meme_ify_me/MainActivity.java
@@ -1,9 +1,19 @@
 package nyc.c4q.rosmaryfc.meme_ify_me;
 
+import android.content.ComponentName;
+import android.content.Intent;
+import android.content.pm.ResolveInfo;
+import android.os.Parcelable;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+
+import java.net.URI;
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -36,4 +46,40 @@ public class MainActivity extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    public void onShareClick(View v){
+               List<Intent> targetShareIntents=new ArrayList<Intent>();
+                Intent shareIntent=new Intent();
+                shareIntent.setAction(Intent.ACTION_SEND);
+                shareIntent.setType("text/plain");
+                List<ResolveInfo> resInfos = getPackageManager().queryIntentActivities(shareIntent, 0);
+                boolean intentSafe = resInfos.size() > 0;
+                if(intentSafe){
+
+                                for(ResolveInfo resInfo : resInfos){
+                                String packageName=resInfo.activityInfo.packageName;
+                                Log.i("Package Name", packageName);
+
+                                            Intent intent=new Intent();
+                                    intent.setComponent(new ComponentName(packageName, resInfo.activityInfo.name));
+                                    intent.setAction(Intent.ACTION_SEND);
+                                    intent.setType("image/jpg");
+                                  //reference hoshiko's code
+                                    intent.putExtra(Intent.EXTRA_STREAM, " imageUri "); //need to update this so that we are using variable imageUri not the name (as per hoshiko's code)
+
+                                   // maybe convert imageUri + userinputted text as a Bitmap.     bmp = Bitmap.createBitmap(imageUri);
+
+                                                    intent.putExtra(Intent.EXTRA_SUBJECT, "Made with Meme-ify Me");
+                                    intent.putExtra(Intent.EXTRA_TEXT, "Check out my new meme!");
+
+                                            intent.setPackage(packageName);
+                                    targetShareIntents.add(intent);
+                            }
+                        Intent chooserIntent=Intent.createChooser(targetShareIntents.remove(0), "Choose app to share");
+                        chooserIntent.putExtra(Intent.EXTRA_INITIAL_INTENTS, targetShareIntents.toArray(new Parcelable[]{}));
+                        startActivity(chooserIntent);
+                    } else {
+                        return;
+                    }
+           }
 }
