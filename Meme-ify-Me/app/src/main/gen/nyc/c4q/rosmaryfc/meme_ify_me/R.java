b9d1f154e266f528c3d277743405ae9b23192a87
hoshikoo <hoshikooki@gmail.com>
2015-05-30 20:29:42 -0400

    remove unnecessary files

--- Meme-ify-Me/app/src/main/gen/nyc/c4q/rosmaryfc/meme_ify_me/R.java
+++
@@ -1,7 +0,0 @@
-/*___Generated_by_IDEA___*/
-
-package nyc.c4q.rosmaryfc.meme_ify_me;
-
-/* This stub is only used by the IDE. It is NOT the R class actually packed into the APK */
-public final class R {
-}
