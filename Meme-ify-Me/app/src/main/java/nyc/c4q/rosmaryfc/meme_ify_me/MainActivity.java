19fd0495d417d53c38565a55c09b9893c631096c
hoshikoo <hoshikooki@gmail.com>
2015-05-30 21:59:33 -0400

    camera function in main activity and manifest

--- Meme-ify-Me/app/src/main/java/nyc/c4q/rosmaryfc/meme_ify_me/MainActivity.java
+++ Meme-ify-Me/app/src/main/java/nyc/c4q/rosmaryfc/meme_ify_me/MainActivity.java
@@ -1,19 +1,78 @@
 package nyc.c4q.rosmaryfc.meme_ify_me;
 
+import android.app.Activity;
+import android.content.ContentResolver;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.media.Image;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.RadioButton;
+import android.widget.Toast;
+
+import java.io.File;
 
 
 public class MainActivity extends ActionBarActivity {
+
+    private static String logtag ="CameraApp8";
+    private static int TAKE_PICTURE =1;
+    private Uri imageUri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button cameraButton = (Button)findViewById(R.id.camera_button);
+        cameraButton.setOnClickListener(cameraListener);
+    }
+
+    private View.OnClickListener cameraListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            takePhoto(v);
+        }
+    };
+
+    private void takePhoto (View v){
+        Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
+        File photo = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES), "picture.jpg");
+        imageUri = Uri.fromFile(photo);
+        intent.putExtra(MediaStore.EXTRA_OUTPUT,imageUri);
+        startActivityForResult(intent, TAKE_PICTURE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent intent){
+        super.onActivityResult(requestCode, resultCode, intent);
+
+        if(resultCode == Activity.RESULT_OK){
+            Uri selectedImage = imageUri;
+            getContentResolver().notifyChange(selectedImage, null);
+
+            ImageView imageview = (ImageView)findViewById(R.id.image);
+            ContentResolver cr = getContentResolver();
+            Bitmap bitmap;
+
+            try{
+                bitmap = MediaStore.Images.Media.getBitmap(cr, selectedImage);
+                imageview.setImageBitmap(bitmap);
+                Toast.makeText(MainActivity.this, selectedImage.toString(), Toast.LENGTH_LONG).show();
+
+            }catch(Exception e){
+                Log.e(logtag, e.toString());
+            }
+        }
     }
 
 
