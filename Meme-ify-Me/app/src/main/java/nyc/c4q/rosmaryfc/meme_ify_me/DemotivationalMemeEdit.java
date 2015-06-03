657298e578a4e93463672953840defe5b08234fa
RosmaryFC <Rosmaryfc@hotmail.com>
2015-06-03 06:02:23 -0400

    preview btns now work in demotivational activity

--- Meme-ify-Me/app/src/main/java/nyc/c4q/rosmaryfc/meme_ify_me/DemotivationalMemeEdit.java
+++ Meme-ify-Me/app/src/main/java/nyc/c4q/rosmaryfc/meme_ify_me/DemotivationalMemeEdit.java
@@ -1,18 +1,50 @@
 package nyc.c4q.rosmaryfc.meme_ify_me;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 
 public class DemotivationalMemeEdit extends ActionBarActivity {
+    private TextView titleTextView;
+    private TextView phraseTextView;
+    private EditText titleEditText;
+    private EditText phraseEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_demotivational_meme_edit);
+
+        Button titleEditTxtPreviewBtn = (Button) findViewById(R.id.title_editText_preview_btn);
+        titleEditTxtPreviewBtn.setOnClickListener(titlePreviewBtnListener);
+
+        Button phraseEditTxtPreviewBtn = (Button) findViewById(R.id.phrase_editText_preview_btn);
+        phraseEditTxtPreviewBtn.setOnClickListener(phrasePreviewBtnListener);
     }
+
+    private View.OnClickListener titlePreviewBtnListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            titleEditText = (EditText) findViewById(R.id.title_editText);
+            titleTextView = (TextView) findViewById(R.id.title_textView);
+            titleTextView.setText(titleEditText.getText().toString());
+        }
+    };
+
+    private View.OnClickListener phrasePreviewBtnListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            phraseEditText = (EditText) findViewById(R.id.phrase_editText);
+            phraseTextView = (TextView) findViewById(R.id.phrase_textView);
+            phraseTextView.setText(phraseEditText.getText().toString());
+        }
+    };
 
 
     @Override
